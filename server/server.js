9fd2d4fd04ca856bc3660962836fbea8bd4fb65f
Alex Brown <alexbrown@macbookpro.lan>
2024-11-02 16:20:51 -0500

    built MongoDB Server Connection and Express install

--- server/server.js
+++ server/server.js
@@ -1,10 +1,32 @@
-const express = require('express');
-const mongoose  = require('mongoose');
+const express = require('express'); // Import Express
+const mongoose  = require('mongoose'); 
 const cookieParser = require('cookie-parser');
-const cors = require(cors);
+const cors = require('cors');
 
-// Create a mongoose DB connection
-mongoose.connect()
+// Create a mongoose DB connection -> I can also Create a Seperate file with this and use import / use in thsi file. 
+mongoose.connect('mongodb+srv://alexartbrown:9254F0rest@cluster0.ijuff.mongodb.net/').then(() =>console.log("Connection to MongoDB Established")).catch((error) => console.log(error));
 
-const app = express ('mongodb+srv://alexartbrown:9254F0rest@cluster0.ijuff.mongodb.net/').then(() =>console.log("Connection to MongoDB Established")).catch((error) => console.log(error));
-const PORT = process.env.PORT || 5000;
+const app = express ();
+const PORT = process.env.PORT || 5001; // Port for backend App Server
+
+// my app will USE (app.USE) the following compnentes. 
+app.use(
+    cors({
+        orgin: 'host://localhost:5173/',
+        mehtods: ['GET', 'POST', 'DELETE', 'PUT'],
+        allowedHeaders: [
+            "Content-Type",
+            'Authorization',
+            'Cache-Control',
+            'Expires',
+            "Pragma"
+        ],
+        credentials :true
+    })
+);
+
+app.use(cookieParser());
+
+app.use(express.json());
+
+app.listen(PORT, () => console.log (`Server is now running on port ${PORT}`))
